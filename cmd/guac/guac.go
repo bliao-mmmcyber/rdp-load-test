3e34cacc69aa89260328a37b2445d18b49565c47
Kchung-appaegis <kchung@appaegis.com>
2021-02-22 16:42:29 +0800

    AC-507 1. append auth token in host header

--- cmd/guac/guac.go
+++ cmd/guac/guac.go
@@ -14,6 +14,7 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -157,13 +158,13 @@ func DemoDoConnect(request *http.Request) (guac.Tunnel, error) {
 	}
 
 	// TODO: AC-507
-	//if v, ok := config.Parameters["hostname"]; ok {
-	//	if strings.Contains(v, "template-rdp") {
-	//		if appauthz, err := request.Cookie("appauthz"); err == nil {
-	//			config.Parameters["hostname"] = v + ";" + appauthz.Value
-	//		}
-	//	}
-	//}
+	if v, ok := config.Parameters["hostname"]; ok {
+		if strings.Contains(v, "template-rdp") {
+			if appauthz, err := request.Cookie("appauthz"); err == nil {
+				config.Parameters["hostname"] = v + ";" + appauthz.Value
+			}
+		}
+	}
 
 	var err error
 	if query.Get("width") != "" {
